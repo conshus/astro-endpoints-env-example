3878aed3dc6615791ec71e3d37dc22abfbfc168d
conshus <dwane.hemmings@vonage.com>
2024-10-01 23:03:47 -0400

    Initial commit

---
+++ src/pages/api/check-password.ts
@@ -0,0 +1,37 @@
+import type { APIRoute } from 'astro';
+const { ADMIN_PASSWORD, HOST_PASSWORD, GUEST_PASSWORD } = import.meta.env;
+
+export const GET: APIRoute = () => {
+  return new Response(
+    JSON.stringify({
+      greeting: 'Hello',
+    })
+  );
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  // console.log('post req: ', await request.json());
+  const data = await request.json();
+  const password = data.password;
+  const role = data.role.toLowerCase();
+  let authorized = false;
+  switch (role) {
+    case 'admin':
+      authorized = password === ADMIN_PASSWORD;
+      break;
+    case 'host':
+      authorized = password === HOST_PASSWORD;
+      break;
+    case 'guest':
+      authorized = password === GUEST_PASSWORD;
+      break;
+    default:
+      authorized = false;
+  }
+
+  return new Response(
+    JSON.stringify({
+      authorized,
+    })
+  );
+};
---
+++ src/pages/api/save-settings.ts
@@ -0,0 +1,2 @@
+import type { APIRoute } from 'astro';
+const { ADMIN_PASSWORD, HOST_PASSWORD, GUEST_PASSWORD } = import.meta.env;
