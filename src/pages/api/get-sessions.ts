78c1e3ed769d25b62f69c3ec862e592e2fd6754f
conshus <dwane.hemmings@vonage.com>
2024-10-01 23:25:33 -0400

    adds github integration

---
+++ src/pages/api/get-sessions.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from 'astro';
+const { GITHUB_PAT } = import.meta.env;
+import { Octokit } from "@octokit/core";
+const octokit = new Octokit({ auth: GITHUB_PAT });
+
+export const GET: APIRoute = async () => {
+    const originalFile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: 'conshus',
+        repo: 'astro-endpoints-env-example',
+        path: 'sessions.json',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+    const sessionsContent = Buffer.from(originalFile.data.content, "base64").toString();
+
+
+    return new Response(
+      JSON.stringify({
+        body: sessionsContent,
+      })
+    );
+  };
+  
