8f6207905b8f5f3391a0f0603ca2516f23e3538c
conshus <dwane.hemmings@vonage.com>
2024-10-02 14:24:49 -0400

    updates sessions endpoint

--- src/pages/api/sessions.ts
+++ src/pages/api/sessions.ts
@@ -26,8 +26,8 @@ export const GET: APIRoute = async () => {
 
 
 export const POST: APIRoute = async ({ request }) => {
-
-  const base64newSessions = Buffer.from(request.body).toString('base64');
+  const data = await request.json();
+  const base64newSessions = Buffer.from(data.body).toString('base64');
 
   const originalFile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
     owner: 'conshus',
